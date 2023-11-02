af3c269ccb659b3f2d39134a2c974424083dff38
Illya <naumenkoilya87@gmail.com>
2023-11-03 01:45:57 +0200

    class Contact done

--- ContactBook/Contact.cpp
+++ ContactBook/Contact.cpp
@@ -1,2 +1,28 @@
 #include "framework.h"
 #include "Contact.h"
+
+Contact::Contact()
+	: name("-"), surname("-"), number("-")
+{}
+
+Contact::Contact(std::string name, std::string surname, std::string number)
+	: name(name), surname(surname), number(number)
+{}
+
+Contact::~Contact()
+{}
+
+std::string Contact::getName() const
+{
+	return name;
+}
+
+std::string Contact::getSurname() const
+{
+	return surname;
+}
+
+std::string Contact::getNumber() const
+{
+	return number;
+}
--- ContactBook/Contact.h
+++ ContactBook/Contact.h
@@ -1,5 +1,16 @@
 #pragma once
+
 class Contact
 {
+	std::string name;
+	std::string surname;
+	std::string number;
+public:
+	Contact();
+	Contact(std::string name, std::string surname, std::string number);
+	~Contact();
+	std::string getName() const;
+	std::string getSurname() const;
+	std::string getNumber() const;
 };
 
