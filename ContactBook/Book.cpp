c3cdfbeac0e08368a362bbce026dd46c7999dade
Illya <naumenkoilya87@gmail.com>
2023-11-04 01:47:43 +0200

    Book.h and Book.cpp done

--- ContactBook/Book.cpp
+++ ContactBook/Book.cpp
@@ -1,2 +1,56 @@
 #include "framework.h"
 #include "Book.h"
+
+Book::Book()
+	: name("-"), text("-"), ngroup(0.0)
+{}
+
+Book::Book(std::string name, std::string text, int ngroup)
+	:name(name), text(text), ngroup(ngroup)
+{}
+
+Book::~Book()
+{}
+
+std::string Book::getName() const
+{
+	return name;
+}
+
+Group* Book::getGroup() const
+{
+	return groups;
+}
+
+std::string Book::getText() const
+{
+	return text;
+}
+
+int Book::getNgroup() const
+{
+	return ngroup;
+}
+
+void Book::addGroup(const Group& group) const
+{
+	Group* g = new Group[ngroup + 1];
+	for (int i = 0; i < ngroup; i++) {
+		g[i] = groups[i];
+	}
+	g[ngroup] = group;
+	delete[] groups;
+}
+
+void Book::deleteG(std::string name)
+{
+	for (int i = 0; i < ngroup; i++) {
+		if (getName() == name) {
+			for (int j = 0; j < ngroup; j++) {
+				groups[j] = groups[j + 1];
+			}
+			ngroup--;
+			break;
+		}
+	}
+}
--- ContactBook/Book.h
+++ ContactBook/Book.h
@@ -1,5 +1,21 @@
 #pragma once
+#include "Group.h"
+
 class Book
 {
+	std::string name;
+	Group* groups;
+	std::string text;
+	int ngroup;
+public:
+	Book();
+	Book(std::string name, std::string text, int ngroup);
+	~Book();
+	std::string getName() const;
+	Group* getGroup() const;
+	std::string getText() const;
+	int getNgroup() const;
+	void addGroup(const Group& group) const;
+	void deleteG(std::string name);
 };
 
